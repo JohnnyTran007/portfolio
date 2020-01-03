22b6c33f5f5498e39e265176f9e7088277224902
John Tran <johnnydattran@gmail.com>
2020-01-03 15:57:11 -0700

    updated LinkedIn link on landingpage

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -23,7 +23,7 @@ class Landing extends Component {
               <div className="social-links">
                 {/* LinkedIn */}
                 <a
-                  href="www.linkedin.com/in/johnnytran007"
+                  href="https://www.linkedin.com/in/johnnytran007/"
                   rel="noopener noreferrer"
                   target="_blank"
                 >
